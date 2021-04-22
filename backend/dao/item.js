e7843aa6a6a635e6418b4a10bb72d66efaccae2b
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-04-22 13:37:00 +0530

    getItems by category route added and theme font changed

--- backend/dao/item.js
+++ backend/dao/item.js
@@ -1,15 +1,18 @@
-const { request, json } = require("express");
-const { RequestTimeout } = require("http-errors");
-var ItemModel = require("../models/item");
+const { request, json } = require('express');
+const { RequestTimeout } = require('http-errors');
+var ItemModel = require('../models/item');
 
 const limit = 15;
 
-
 const itemDao = {
-
   async getItemById(id) {
-    console.log(id)
+    console.log(id);
     return await ItemModel.findOne({ id });
+  },
+
+  async getItems(category) {
+    console.log(category);
+    return await ItemModel.find({});
   },
 };
 
--- backend/routes/itemRouter.js
+++ backend/routes/itemRouter.js
@@ -1,20 +1,33 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var axios = require("axios");
-var multer = require("multer");
-var FormData = require("form-data");
-var fs = require("fs");
+var axios = require('axios');
+var multer = require('multer');
+var FormData = require('form-data');
+var fs = require('fs');
 
-var ItemDao = require("../dao/item");
+var ItemDao = require('../dao/item');
 
-router.get("/item/:id", async (req, res, next) => {
+// Public
+// GET Single item based on ID
+router.get('/item/:id', async (req, res, next) => {
   const itemid = req.params.id;
   try {
     const data = await ItemDao.getItemById(itemid);
     return res.status(200).send(data);
   } catch (error) {
-    return res.status(400).send("error");
+    return res.status(400).send('error');
   }
 });
 
+// Public
+// GET All Items based on category
+router.get('/items/:category', async (req, res, next) => {
+  const category = req.params.category;
+  try {
+    const data = await ItemDao.getItems(category);
+    return res.status(200).send(data);
+  } catch (error) {
+    return res.status(400).send('error');
+  }
+});
 module.exports = router;
