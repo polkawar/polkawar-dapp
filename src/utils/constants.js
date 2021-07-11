46701852950434d11e79311b09591f9f33a978ae
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-07-11 09:25:12 +0530

    contract updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,14 +5,14 @@ if ((process.env.NODE_ENV === 'production')) {
   constants = {
     network_id: '56',
     network_address: 'https://bsc-dataseed.binance.org/',
-    saleContractAddress: '0x04192444532e6117Df256d0c6AB8cEaaA3705D48', //This will be the owner
+    saleContractAddress: '0x268A31FFC318f0603b9f4B11122291869E1EC678', //This will be the owner
     itemContractAddress: '0x44EeE203F8aD35dA2F8B30c74A3F291FaebF97b1'
   };
 } else {
   constants = {
     network_id: '97',
     network_address: 'https://bsc-dataseed.binance.org/',
-    saleContractAddress: '0x04192444532e6117Df256d0c6AB8cEaaA3705D48',
+    saleContractAddress: '0x268A31FFC318f0603b9f4B11122291869E1EC678',
     itemContractAddress: '0x44EeE203F8aD35dA2F8B30c74A3F291FaebF97b1'
 
   };
