6ed2df6a91247b11bc72fef3f7de7c79c85d9c76
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-07-11 10:46:53 +0530

    mainnet contract added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,8 +5,8 @@ if ((process.env.NODE_ENV === 'production')) {
   constants = {
     network_id: '56',
     network_address: 'https://bsc-dataseed.binance.org/',
-    saleContractAddress: '0x772611731D3C011ba282E400A056681b96B55783', //This will be the owner
-    itemContractAddress: '0x44EeE203F8aD35dA2F8B30c74A3F291FaebF97b1'
+    saleContractAddress: '0xdcAD5608a4ec6b5146B1873c2C9AEA19B329769C', //This will be the owner
+    itemContractAddress: '0x68899569f9D4d3Ec7D947691BaD4B93ee418926F'
   };
 } else {
   constants = {
