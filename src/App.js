1acb567833a0b0699c4beef6452364d5167cc64a
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-07-12 13:25:13 +0530

    UI design for bid page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import Appbar from './common/Appbar';
-import Footer from './common/Footer';
 import Home from './pages/home/Home';
 import './App.css';
 import { ThemeProvider } from '@material-ui/core/styles';
@@ -11,18 +10,17 @@ import Details from './pages/details/Details';
 import { Provider } from 'react-redux';
 import store from './store';
 import Airdrop from './pages/airdrop/Airdrop';
-import SocialLinks from './common/SocialLinks';
-import './web';
 import Sale from './pages/sale/Sale';
+import Bid from './pages/bid/Bid';
 
 export default function App() {
-  
+
   return (
     <Provider store={store}>
       <Router>
         <ThemeProvider theme={theme}>
           <Fragment>
-         
+
             <Appbar />
             <Switch>
               <Route exact path="/">
@@ -34,6 +32,10 @@ export default function App() {
               <Route exact path="/sale">
                 <Sale />
               </Route>
+              <Route exact path="/bid">
+                <Bid />
+              </Route>
+
               <Route exact path="/item/:id">
                 <Details />
               </Route>
