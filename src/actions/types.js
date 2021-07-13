8a15a5ac978e7f9d260da798daf35f5475d12c99
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-07-13 09:08:29 +0530

    bid page UI creates

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,13 +1,8 @@
 export const GET_ERRORS = 'GET_ERRORS';
 
-//Items reducers
-export const GET_ITEMS_CATEGORY = 'GET_ITEMS_CATEGORY';
-export const GET_ITEMS = 'GET_ITEMS';
-export const GET_FLASH_ITEMS = 'GET_FLASH_ITEMS';
-export const ADD_USER_ITEM = 'ADD_USER_ITEM';
-export const GET_USER_ITEMS = 'GET_USER_ITEMS';
-export const UPDATE_USER_ITEM_OWNER = 'UPDATE_USER_ITEM_OWNER';
-export const GET_ITEM = 'GET_ITEM';
+//Authentication reducers
+export const GET_CURRENT_USER = 'GET_CURRENT_USER';
+export const REMOVE_CURRENT_USER = 'REMOVE_CURRENT_USER';
 
 //Character reducers
 export const GET_CHARACTERS = 'GET_CHARACTERS';
@@ -15,6 +10,16 @@ export const GET_CHARACTERS = 'GET_CHARACTERS';
 //User reducers
 export const GET_USER = 'GET_USER';
 
-//Authentication reducers
-export const GET_CURRENT_USER = 'GET_CURRENT_USER';
-export const REMOVE_CURRENT_USER = 'REMOVE_CURRENT_USER';
+//Bids Reducers
+export const GET_BID_ITEM_BY_ID = 'GET_BID_ITEM_BY_ID';
+
+//Items reducers
+export const GET_ITEMS_CATEGORY = 'GET_ITEMS_CATEGORY';
+export const GET_ITEMS = 'GET_ITEMS';
+
+//Sale reducers
+export const GET_FLASH_ITEMS = 'GET_FLASH_ITEMS';
+export const ADD_USER_ITEM = 'ADD_USER_ITEM';
+export const GET_USER_ITEMS = 'GET_USER_ITEMS';
+export const UPDATE_USER_ITEM_OWNER = 'UPDATE_USER_ITEM_OWNER';
+export const GET_ITEM = 'GET_ITEM';
