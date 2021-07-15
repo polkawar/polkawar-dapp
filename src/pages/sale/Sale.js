c1d734751d72abbb7a877b617d6ce66b73a23d6d
jackson <polkawar.official@gmail.com>
2021-07-15 10:10:36 +0700

    fix

--- src/pages/sale/Sale.js
+++ src/pages/sale/Sale.js
@@ -280,11 +280,11 @@ function FlashSale({ getFlashItems, getUserItems, flash, useritems }) {
 									<li className={classes.listItem}>
 										Reselling of the NFT Item will start from{' '}
 										<span style={{ color: 'yellow' }}>
-											<Moment format="DD-MM-YYYY HH:mm">{resaleStartDate}</Moment>
+											<Moment format="DD-MM-YYYY HH:mm">{resaleStartDate}</Moment> UTC
 										</span>{' '}
 										and will end{' '}
 										<span style={{ color: 'yellow' }}>
-											<Moment format="DD-MM-YYYY HH:mm">{resaleEndDate}</Moment>.
+											<Moment format="DD-MM-YYYY HH:mm">{resaleEndDate}</Moment> UTC.
 										</span>
 									</li>
 									<li className={classes.listItem}>
