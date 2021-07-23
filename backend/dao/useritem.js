bd333db5d3e36167c66c5405de5f9d7f1313de86
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-07-23 12:14:58 +0530

    Changes

--- backend/dao/useritem.js
+++ backend/dao/useritem.js
@@ -36,7 +36,7 @@ const userItemDao = {
 		// return await UserItemModel.find({});
 		//After testing delete items
 		//return await UserItemModel.remove({ event: 'auction' });
-		return await UserItemModel.remove({ event: 'auction-reward' });
+		//return await UserItemModel.remove({ event: 'auction-reward' });
 	},
 };
 
