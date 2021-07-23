2a081fd81c7c349f284e30caef4dc7265c6ba6ed
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-07-23 16:46:31 +0530

    mainnet added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 let constants;
 constants = {
-	network: 1, // 0 mainnet, 1 testnet
+	network: 0, // 0 mainnet, 1 testnet
 	network_id: '56',
 	network_address: 'https://bsc-dataseed.binance.org/',
 	saleContractAddress: '0xdcAD5608a4ec6b5146B1873c2C9AEA19B329769C', //This will be the owner
