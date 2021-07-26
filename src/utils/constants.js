e24171758e0a850d30cd26f68a8715067f3c68e2
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-07-26 08:23:06 +0530

    file setup

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 let constants;
 constants = {
-	net: 0, // 0 mainnet, 1 testnet
+	net: 1, // 0 mainnet, 1 testnet
 	network_id: '56',
 	network_address: 'https://bsc-dataseed.binance.org/',
 	saleContractAddress: '0xdcAD5608a4ec6b5146B1873c2C9AEA19B329769C', //This will be the owner
