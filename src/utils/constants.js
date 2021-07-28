1ba99a6ecae9ac13ce323c261497fa46e914794f
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-07-28 10:00:02 +0530

    constants changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ let network_type = 1;
 let constants;
 constants = {
 	net: network_type,
-	sale_owner_address: process.env.REACT_APP_SALE_OWNER_ADDRESS_APPROVE, //Owner address for NFT Items Approve Fn
+	sale_owner_address: process.env.REACT_APP_SALE_APPROVE_OWNER_ADDRESS, //Owner address for NFT Items Approve Fn
 };
 
 export default constants;
