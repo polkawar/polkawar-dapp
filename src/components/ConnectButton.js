2e9764f41d7c21c540941a0af8bde520a39c7db0
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-07-28 09:39:04 +0530

    code cleanup

--- src/components/ConnectButton.js
+++ src/components/ConnectButton.js
@@ -5,69 +5,67 @@ import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import { authenticateUser } from './../actions/authActions';
 import constants from './../utils/constants';
-import web3 from './../web';
+import { checkCorrectNetwork, checkWalletAvailable } from '../actions/web3Actions';
 
 const useStyles = makeStyles((theme) => ({
-  button: {
-    color: 'white',
-    backgroundColor: 'white',
-    textTransform: 'none',
-    borderRadius: '50px',
-    padding: '8px 16px 8px 16px',
-    fontWeight: 600,
-    background: `linear-gradient(to bottom,#D9047C, #BF1088)`,
-    fontSize: 14,
-  },
+	button: {
+		color: 'white',
+		backgroundColor: 'white',
+		textTransform: 'none',
+		borderRadius: '50px',
+		padding: '8px 16px 8px 16px',
+		fontWeight: 600,
+		background: `linear-gradient(to bottom,#D9047C, #BF1088)`,
+		fontSize: 14,
+	},
 }));
 
 function ConnectButton({ authenticateUser }) {
-  const classes = useStyles();
-  const [error, setError] = useState('');
+	const classes = useStyles();
+	const [ error, setError ] = useState('');
 
-  const checkNetwork = () => {
-    if (window.ethereum.networkVersion === constants.network_id) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  const connectWallet = async () => {
-    if (checkNetwork()) {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+	const connectWallet = async () => {
+		if (window.ethereum !== undefined) {
+			const networkStatus = await checkCorrectNetwork();
+			if (networkStatus) {
+				const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+				const accountAddress = accounts[0];
 
-      const accountAddress = accounts[0];
-      authenticateUser(accountAddress);
-      setError('');
-    } else {
-      setError('Only support BSC network');
-    }
-  };
+				authenticateUser(accountAddress);
+				setError('');
+			} else {
+				setError('Only support BSC network');
+			}
+		} else {
+			setError('Wallet Not Available.');
+		}
+	};
 
-  return (
-    <div className="my-5 text-center">
-      <div className="mt-5 text-center">
-        <h4 style={{ color: 'yellow' }}>Missing Wallet!</h4>
-        <p style={{ color: 'white' }}>Connect your wallet first and then only you can claim airdrop.</p>
-      </div>
-      <div className="mt-5">
-        <Button className={classes.button} onClick={connectWallet}>
-          Connect your wallet
-        </Button>
-        <div className="mt-2" style={{ color: 'yellow' }}>
-          {error}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="my-5 text-center">
+			<div className="mt-5 text-center">
+				<h4 style={{ color: 'yellow' }}>Missing Wallet!</h4>
+				<p style={{ color: 'white' }}>Connect your wallet first and then only you can claim airdrop.</p>
+			</div>
+			<div className="mt-5">
+				<Button className={classes.button} onClick={connectWallet}>
+					Connect your wallet
+				</Button>
+				<div className="mt-2" style={{ color: 'yellow' }}>
+					{error}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 ConnectButton.propTypes = {
-  authenticateUser: propTypes.func.isRequired,
+	authenticateUser: propTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  authenticated: state.auth.authenticated,
-  user: state.auth.user,
+	authenticated: state.auth.authenticated,
+	user: state.auth.user,
 });
 
 const mapDispatchToProps = { authenticateUser };
