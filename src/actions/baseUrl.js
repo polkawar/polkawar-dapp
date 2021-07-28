5da258b575cf1d01a4bf6cb4efdead6fcac79bc4
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-07-28 16:53:34 +0530

    baseUrl changes

--- src/actions/baseUrl.js
+++ src/actions/baseUrl.js
@@ -1,4 +1,4 @@
 //const baseUrl = 'https://api.polkawar.com';
 // const baseUrl = process.env.REACT_APP_BACKEND_BASE_URL;
-const baseUrl = 'https://api.polkawar.com';
+const baseUrl = process.env.REACT_APP_BACKEND_BASE_URL;
 export default baseUrl;
