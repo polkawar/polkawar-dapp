6ac007d1c3d3901138e104bba25c8c9b3fa3d768
Tahir Ahmad <tahirahmad.sms@gmail.com>
2021-08-02 11:27:31 +0530

    items data updated

--- backend/models/useritem.js
+++ backend/models/useritem.js
@@ -1,40 +1,40 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var UserItemModel = new mongoose.Schema({
-	tokenId: {
-		type: String,
-		required: true,
-	},
-	tokenType: {
-		type: Number,
-		required: true,
-	},
-	price: {
-		type: String,
-		required: false,
-	},
-	pId: {
-		type: String,
-		required: false,
-	},
-	comboId: {
-		type: Number,
-		required: false,
-	},
-	event: {
-		type: String,
-		required: true,
-	},
-	owner: {
-		type: String,
-		required: true,
-	},
-	buyDate: {
-		type: Date,
-		required: true,
-		default: new Date(),
-	},
+  tokenId: {
+    type: String,
+    required: true,
+  },
+  tokenType: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: false,
+  },
+  pId: {
+    type: String,
+    required: false,
+  },
+  comboId: {
+    type: Number,
+    required: false,
+  },
+  event: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: String,
+    required: true,
+  },
+  buyDate: {
+    type: Date,
+    required: true,
+    default: new Date(),
+  },
 });
-const UserItem = mongoose.model('UserItem', UserItemModel, 'UserItem');
+const UserItem = mongoose.model("UserItem", UserItemModel, "UserItem");
 
 module.exports = UserItem;
