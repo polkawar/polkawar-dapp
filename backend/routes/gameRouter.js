636f07a936092a8348aa94174048d9c2f3f3bfc1
Tahir Ahmad <tahirahmad.in@gmail.com>
2022-02-15 10:30:04 +0530

    catch block changes

--- backend/routes/gameRouter.js
+++ backend/routes/gameRouter.js
@@ -1,9 +1,5 @@
 var express = require("express");
 var router = express.Router();
-var axios = require("axios");
-var multer = require("multer");
-var FormData = require("form-data");
-var fs = require("fs");
 const characterHelper = require("../helper/characterHelper");
 
 // POST create new character based on details
@@ -15,22 +11,21 @@ router.post("/update-status", async (req, res, next) => {
 
     if (parseInt(poolId) > 0 && winnerAddress !== null) {
       data = await characterHelper.updateStatus(winnerAddress, poolId);
-      console.log(data);
+
       return res.status(200).send(data);
     } else {
       return res.status(200).send("either pool_id or winner address is wrong.");
     }
   } catch (error) {
-    console.log(error);
-    return res.status(400).send(String(error));
+    return res.status(400).send(error);
   }
 });
-// GET create new character based on details
+// GET status test
 router.get("/status", async (req, res, next) => {
   try {
     return res.status(200).send("Working");
   } catch (error) {
-    return res.status(400).send(String(error));
+    return res.status(400).send(error);
   }
 });
 
